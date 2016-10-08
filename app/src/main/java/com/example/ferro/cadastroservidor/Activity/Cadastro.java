0adf273b3867595a65c99ac8c83cd40b00047136
ArthurFerrao <arthurdeferrao@gmail.com>
2016-10-08 00:09:26 -0300

    remover botão

--- app/src/main/java/com/example/ferro/cadastroservidor/Activity/Cadastro.java
+++ app/src/main/java/com/example/ferro/cadastroservidor/Activity/Cadastro.java
@@ -1,11 +1,9 @@
 package com.example.ferro.cadastroservidor.Activity;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
-import com.example.ferro.cadastroservidor.Listeners.onClickVoltar;
 import com.example.ferro.cadastroservidor.R;
 
 public class Cadastro extends Activity {
@@ -15,8 +13,5 @@ public class Cadastro extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
 
-        Button btVoltar = (Button) findViewById(R.id.btVoltar);
-
-        btVoltar.setOnClickListener(new onClickVoltar(this));
     }
 }
